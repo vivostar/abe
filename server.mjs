f37b135910e305b24f52a732f6c1165f5b5bd880
vivostar <lishuaipeng651@gmail.com>
2022-08-01 03:53:24 -0400

    change spark-submit command

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ app.use(json())
 //get all members
 app.get('/api/create', (req, res) => {
 
-  let spark_submit_cmd = `/opt/spark/bin/spark-submit `
+  let spark_submit_cmd = `spark-submit `
   spark_submit_cmd = `${spark_submit_cmd}--class ${req.query['class']} `
   for (let config of req.body['conf']) {
     spark_submit_cmd = spark_submit_cmd + '--conf ' + config['configName'] + '=' + config['configValue'] + ' '
@@ -15,7 +15,8 @@ app.get('/api/create', (req, res) => {
   spark_submit_cmd = spark_submit_cmd + req.body['jar'] + ' ' + req.body['args']
   console.log(spark_submit_cmd)
   const test = shell.exec(spark_submit_cmd)
-  res.json({"test": {"output": test}})
+  console.log(test.grep('Pi'))
+  res.json({"test": {"output": test.grep('Pi')}})
 })
 
 const PORT = process.env.PORT || 5000
